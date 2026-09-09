186f3f1eb044b4c43f856a6689018f37e5797016
Kassson <73960282+Kassson@users.noreply.github.com>
2026-09-10 00:21:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ==================== КОНФИГУРАЦИЯ ====================
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbx3W6c4e6AEzwVk7Ek8C7vqPJ0DfPsirDvAQ7D4JMc4KomULqz9Cs2youftBwr1F_Uw/exec',
+    API_URL: 'https://corsproxy.io/?' + encodeURIComponent('https://script.google.com/macros/s/AKfycbx3W6c4e6AEzwVk7Ek8C7vqPJ0DfPsirDvAQ7D4JMc4KomULqz9Cs2youftBwr1F_Uw/exec'),
     
     NOTIFICATIONS: {
         MORNING: '07:30',
