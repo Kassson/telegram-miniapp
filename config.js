37bd1ee050df591144718c957afcd02b31531732
Kassson <73960282+Kassson@users.noreply.github.com>
2026-09-10 00:31:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 // ==================== КОНФИГУРАЦИЯ ====================
 const CONFIG = {
-    // ИСПОЛЬЗУЕМ ПРОКСИ ДЛЯ ОБХОДА CORS
-    // Прокси получает данные с вашего скрипта и возвращает их без CORS блокировки
-    API_URL: 'https://api.allorigins.win/raw?url=' + encodeURIComponent('https://script.google.com/macros/s/AKfycbx3W6c4e6AEzwVk7Ek8C7vqPJ0DfPsirDvAQ7D4JMc4KomULqz9Cs2youftBwr1F_Uw/exec'),
+    API_URL: 'https://script.google.com/macros/s/AKfycbx3W6c4e6AEzwVk7Ek8C7vqPJ0DfPsirDvAQ7D4JMc4KomULqz9Cs2youftBwr1F_Uw/exec',
     
     NOTIFICATIONS: {
         MORNING: '07:30',
